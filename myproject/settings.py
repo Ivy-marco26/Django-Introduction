9d2abeb6834423d25e0238c3707cf855f94e1a93
margon-glitch <marco15gonzalez@gmail.com>
2025-04-22 21:12:51 -0400

    Initial Django tutorial project with working blog and HTML

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    'myapp',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog',  # ← your app
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
